b44b27141584903fb43026e89e231030e6423408
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-08-03 09:37:48 +0200

    fix(barreredaction): use _publication_button.twig

--- actions/barreredaction__.php
+++ actions/barreredaction__.php
@@ -14,4 +14,4 @@ $plugin_output_new = preg_replace(
     '</div>',
     $plugin_output_new
 );
-$plugin_output_new = preg_replace('#</div>#', '<a class="link-pdf" href="'.$this->href('pdf').'" title="'._t('PUBLICATION_EXPORT_PAGE_TO_PDF').'" onclick="toastMessage(_t(\'PUBLICATION_PDF_GENERATION_LANCHED\'),7000,\'alert alert-primary\');"><i class="glyphicon glyphicon-book"></i> '. _t('PUBLICATION_DOWNLOAD_PDF') .'</a>'."\n".'</div>', $plugin_output_new);
+$plugin_output_new = preg_replace('#</div>#', $this->render('@bazar/entries/_publication_button.twig', ['forPage'=>true])."\n".'</div>', $plugin_output_new);
